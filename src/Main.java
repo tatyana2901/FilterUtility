0b0ab05009037f78699c1ed7966ad2f1520d02c8
tatiana <tznatkova90@gmail.com>
2025-02-14 15:11:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Utility ut = new Utility();
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.equals("-a")) {
+                ut.setIfAppend(true);
+            }
+            if (arg.endsWith(".txt")) {
+                ut.addFileToList(arg);
+            }
+            if (arg.equals("-p")) {
+                try {
+                    ut.addPrefixToFilenames(args[i + 1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Не введено значение реквизита -p (префикс имени файла)!");
+                }
+            }
+            if (arg.equals("-o")) {
+                try {
+                    ut.setPath(args[i + 1]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Не введено значение реквизита -o (путь)!");
+                }
+            }
+            if (arg.equals("-s")) {
+                ut.setStatisticShort(true);
+            }
+            if (arg.equals("-f")) {
+                ut.setStatisticFull(true);
+            }
+            if (i == args.length - 1 && ut.getInputFiles().isEmpty()) {
+                System.out.println("Не введено ни одного файла для чтения.");
+                break;
+            }
+        }
+
+        ut.doCommand();
+
+
+    }
+
+}
---
+++ src/Utility.java
@@ -0,0 +1,168 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utility {
+
+    private String stringFilename = "strings.txt";
+    private String floatFilename = "floats.txt";
+    private String intFilename = "integers.txt";
+    private String path;
+    private boolean ifAppend;
+    private boolean isStatisticShort;
+    private boolean isStatisticFull;
+    private List<String> inputFiles = new ArrayList<>();
+
+    public void addFileToList(String filename) {
+        inputFiles.add(filename);
+    }
+
+    public List<String> getInputFiles() {
+        return inputFiles;
+    }
+
+    public void setStatisticShort(boolean statisticShort) {
+        isStatisticShort = statisticShort;
+    }
+
+    public void setStatisticFull(boolean statisticFull) {
+        isStatisticFull = statisticFull;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void addPrefixToFilenames(String prefix) {
+        this.stringFilename = new StringBuilder(stringFilename).insert(stringFilename.indexOf("strings.txt"), prefix).toString();
+        this.floatFilename = new StringBuilder(floatFilename).insert(floatFilename.indexOf("floats.txt"), prefix).toString();
+        this.intFilename = new StringBuilder(intFilename).insert(intFilename.indexOf("integers.txt"), prefix).toString();
+    }
+
+    public void setIfAppend(boolean ifAppend) {
+        this.ifAppend = ifAppend;
+    }
+
+    public void deleteFileIfEmpty(File file) {
+        if (file.length() == 0) {
+            file.delete();
+        }
+    }
+
+    public void doCommand() {
+
+        int strCounter = 0;
+        int flCounter = 0;
+        int intCounter = 0;
+
+        double minFl = Double.MAX_VALUE;
+        double maxFl = -Double.MAX_VALUE;
+        double sumFl = 0.0;
+
+        long minInt = Long.MAX_VALUE;
+        long maxInt = Long.MIN_VALUE;
+        long sumInt = 0L;
+
+        int maxStrLength = 0;
+        int minStrLength = Integer.MAX_VALUE;
+
+
+        File intFile = new File(path, intFilename);
+        File floatFile = new File(path, floatFilename);
+        File stringFile = new File(path, stringFilename);
+
+        FileWriter fw = null;
+        FileWriter fw1 = null;
+        FileWriter fw2 = null;
+        try {
+            fw = new FileWriter(intFile, ifAppend);
+            fw1 = new FileWriter(floatFile, ifAppend);
+            fw2 = new FileWriter(stringFile, ifAppend);
+
+            for (int i = 0; i < inputFiles.size(); i++) {
+                BufferedReader bfr = new BufferedReader(new FileReader(inputFiles.get(i), StandardCharsets.UTF_8));
+                while (bfr.ready()) {
+                    String line = bfr.readLine();
+                    try {
+                        long l = Long.parseLong(line);
+                        fw.write(line + "\n");
+                        intCounter++;
+                        sumInt = sumInt + l;
+                        if (l > maxInt) {
+                            maxInt = l;
+                        }
+                        if (l < minInt) {
+                            minInt = l;
+                        }
+                    } catch (NumberFormatException e) {
+                        try {
+                            double d = Double.parseDouble(line);
+                            fw1.write(line + "\n");
+                            flCounter++;
+                            sumFl = sumFl + d;
+                            if (d > maxFl) {
+                                maxFl = d;
+                            }
+                            if (d < minFl) {
+                                minFl = d;
+                            }
+                        } catch (NumberFormatException ex) {
+                            fw2.write(line + "\n");
+                            strCounter++;
+                            if (line.length() > maxStrLength) {
+                                maxStrLength = line.length();
+                            }
+                            if (line.length() < minStrLength) {
+                                minStrLength = line.length();
+                            }
+                        }
+                    }
+                }
+                bfr.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (fw != null) {
+                    fw.close();
+                }
+                if (fw1 != null) {
+                    fw1.close();
+                }
+                if (fw2 != null) {
+                    fw2.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        deleteFileIfEmpty(intFile);
+        deleteFileIfEmpty(floatFile);
+        deleteFileIfEmpty(stringFile);
+
+        if (isStatisticShort || isStatisticFull) {
+            if (intCounter != 0) {
+                System.out.println("В файл " + intFile.getAbsolutePath() + " записано " + intCounter + " элементов.");
+                if (isStatisticFull) {
+                    System.out.println("Максимальное значение: " + maxInt + "; " + "Минимальное значение: " + minInt + "; " + "Сумма: " + sumInt + "; " + "Среднее " + sumInt / intCounter + ".");
+                }
+            }
+            if (flCounter != 0) {
+                System.out.println("В файл " + floatFile.getAbsolutePath() + " записано " + flCounter + " элементов.");
+                if (isStatisticFull) {
+                    System.out.println("Максимальное значение: " + maxFl + "; " + "Минимальное значение: " + minFl + "; " + "Сумма: " + sumFl + "; " + "Среднее " + sumFl / flCounter + ".");
+                }
+            }
+            if (strCounter != 0) {
+                System.out.println("В файл " + stringFile.getAbsolutePath() + " записано " + strCounter + " элементов.");
+                if (isStatisticFull) {
+                    System.out.println("Размер самой длинной строки: " + maxStrLength + "; " + "Размер самой короткой строки: " + minStrLength + ".");
+                }
+            }
+        }
+
+    }
+}
